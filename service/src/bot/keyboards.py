541d3391a7223a2cc6a5a31b299f8dd222c8f169
UlianaL13 <uliana912006@gmail.com>
2025-06-27 17:57:17 +0300

    Add files via upload

---
+++ service/src/bot/keyboards.py
@@ -0,0 +1,13 @@
+from aiogram.types import (ReplyKeyboardMarkup, InlineKeyboardMarkup, KeyboardButton, InlineKeyboardButton)
+
+
+main = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Распознать песню')],
+                           [KeyboardButton(text='SOS')]], 
+                           resize_keyboard=True,
+                           input_field_placeholder='Выберите кнопку...')
+
+type_model = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='простейшая модель', callback_data='model_1')],
+                            [InlineKeyboardButton(text='ускоренная модель', callback_data='model_2')],
+                            [InlineKeyboardButton(text='модель "поиск соседей"', callback_data='model_3')],
+                            [InlineKeyboardButton(text='shazam-like модель', callback_data='model_4')]],
+                            resize_keyboard=True)
